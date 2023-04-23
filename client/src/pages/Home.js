fd8ce86ba29e2207a511f45274d3d6e52fdd6222
AznRaven <74807576+AznRaven@users.noreply.github.com>
2023-04-23 08:08:08 -0500

    home btns size/alignment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h1>Hello! My name is Hai Hoang.</h1>
+      <h1>Hai D. Hoang</h1>
+      <br/>
       <h3>
-        I'm a full stack web developer with a passion for creating and solving problems through web development.
+        I'm a full stack web developer with a passion for
+        <br /> creating and solving problems through web development.
       </h3>
       {/* <div className='bg'>
         <img
@@ -14,57 +16,64 @@ export default function Home() {
       </div> */}
       <br />
       <br />
-      <div className="btn-group" role="group">
-        <Link to="https://www.linkedin.com/in/aznraven/" target="_blank">
-          <button
-            type="button"
-            className="btn btn-secondary"
-            style={{ width: '150px', marginRight: '10px' }}
-          >
-            <img
-              src={
-                'https://res.cloudinary.com/ddl0mgnds/image/upload/v1681009128/Portfolio/icons8-linkedin-48_b6becc.png'
+      <div className>
+        <div className="row d-flex justify-content-center">
+          <div className="col-sm-12 col-lg-4 homebtn m-3">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/aznraven/", "_blank")
               }
-              alt="LinkedIn"
-            ></img>
-            LinkedIn
-          </button>
-        </Link>
-        <Link to="https://github.com/AznRaven" target="_blank">
-          <button
-            type="button"
-            className="btn btn-secondary"
-            style={{ width: '150px', marginLeft: '10px', marginRight: '10px' }}
-          >
-            <img
-              src={
-                'https://res.cloudinary.com/ddl0mgnds/image/upload/v1681008367/Portfolio/icons8-github-48_mh4i7k.png'
+            >
+              <img
+                src={
+                  "https://res.cloudinary.com/ddl0mgnds/image/upload/v1681009128/Portfolio/icons8-linkedin-48_b6becc.png"
+                }
+                alt="LinkedIn"
+              ></img>
+              LinkedIn
+            </button>
+          </div>
+          <div className="col-sm-12 col-lg-4 homebtn m-3">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() =>
+                window.open("https://github.com/AznRaven", "_blank")
               }
-              alt="GitHub"
-            ></img>
-            GitHub
-          </button>
-        </Link>
+            >
+              <img
+                src={
+                  "https://res.cloudinary.com/ddl0mgnds/image/upload/v1681008367/Portfolio/icons8-github-48_mh4i7k.png"
+                }
+                alt="GitHub"
+              ></img>
+              GitHub
+            </button>
+          </div>
+          <div className="col-sm-12 col-lg-4 homebtn m-3">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() =>
+                window.open(
+                  "https://docs.google.com/document/d/17xRo62wEjg8YQbgbZ_eS0JkqOxG4RY9AXl8_2lhtchY/edit?usp=sharing",
+                  "_blank"
+                )
+              }
+            >
+              <img
+                src={
+                  "https://res.cloudinary.com/ddl0mgnds/image/upload/v1681033283/Portfolio/icons8-resume-48_vcjlyl.png"
+                }
+                alt="Resume"
+              ></img>
+              Resume
+            </button>
+          </div>
+        </div>
       </div>
-      <button
-        type="button"
-        className="btn btn-secondary"
-        style={{ width: '150px', marginLeft: '10px' }}
-        onClick={() =>
-          window.open(
-            'https://docs.google.com/document/d/17xRo62wEjg8YQbgbZ_eS0JkqOxG4RY9AXl8_2lhtchY/edit?usp=sharing',
-            '_blank'
-          )
-        }
-      >
-        <img
-          src={
-            'https://res.cloudinary.com/ddl0mgnds/image/upload/v1681033283/Portfolio/icons8-resume-48_vcjlyl.png'
-          }
-          alt="Resume"
-        ></img>
-        Resume
-      </button>
     </>
   );
 }
