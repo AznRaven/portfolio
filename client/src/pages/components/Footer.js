e04c4e95757364e8f41de9440aa8cc878e6b0e16
AznRaven <74807576+AznRaven@users.noreply.github.com>
2023-04-26 15:55:49 -0500

    footer completed

--- client/src/pages/components/Footer.js
+++ client/src/pages/components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+
   return (
     <>
-      <div className="fixed-bottom">Copyright </div>
+      <div className="fixed-bottom bg-dark">© Copyright {currentYear} AznRaven. All rights reserved. </div>
     </>
   );
 }
